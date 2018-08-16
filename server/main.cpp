e19c67c68f78368fe92a8ec544efae4d7320fade
yangdianxp <yangdianxp@163.com>
2018-08-16 19:59:28 +0800

    add some

--- server/main.cpp
+++ server/main.cpp
@@ -2,51 +2,22 @@
 
 int main(int argc, char* argv[])
 {
+	try
+	{
+		if (argc != 2)
+		{
+			std::cerr << "Usage: async_tcp_echo_server <port>\n";
+			return 1;
+		}
+	}
+	catch (std::exception& e)
+	{
+		std::cerr << "Exception: " << e.what() << "\n";
+	}
 
-
+#ifdef _WIN32
+	system("pause");
+#endif
 	return 0;
 }
 
-#if 0
-boost::asio::associated_executor_t<Handler> a = boost::asio::get_associated_executor(h);
-class my_handler
-{
-public:
-	// Custom implementation of Executor type requirements.
-	typedef my_executor executor_type;
-
-	// Return a custom executor implementation.
-	executor_type get_executor() const noexcept
-	{
-		return my_executor();
-	}
-
-	void operator()() { ... }
-};
-
-struct my_handler
-{
-	void operator()() { ... }
-};
-
-namespace boost {
-	namespace asio {
-
-		template <class Executor>
-		struct associated_executor<my_handler, Executor>
-		{
-			// Custom implementation of Executor type requirements.
-			typedef my_executor type;
-
-			// Return a custom executor implementation.
-			static type get(const my_handler&,
-				const Executor& = Executor()) noexcept
-			{
-				return my_executor();
-			}
-		};
-
-	}
-} // namespace boost::asio
-
-#endif
